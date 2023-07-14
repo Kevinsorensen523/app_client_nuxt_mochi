b56f8888b2019fbad54671adf28d6887994958be
Kevinsorensen523 <94046712+Kevinsorensen523@users.noreply.github.com>
2023-07-15 01:18:37 +0700

    Update Testimoni
    add photo & judul

---
+++ components/Testimoni.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="testimoni">
+        <div class="judul text-center text-4xl">
+            <strong><span style="color: #67002B">What our customers say</span>
+            <br />
+            <span style="color: #67002B">about our </span>
+            <span style="color: #FAFAFA">sweets</span></strong>
+        </div>
+      <div class="photo-row mt-10">
+        <img src="~/assets/Testimoni/testimoni1.png" alt="Testimonial 1" />
+        <img src="~/assets/Testimoni/testimoni2.png" alt="Testimonial 2" />
+        <img src="~/assets/Testimoni/testimoni3.png" alt="Testimonial 3" />
+        <img src="~/assets/Testimoni/testimoni4.png" alt="Testimonial 4" />
+        <img src="~/assets/Testimoni/testimoni5.png" alt="Testimonial 5" />
+        <img src="~/assets/Testimoni/testimoni6.png" alt="Testimonial 6" />
+      </div>
+    </div>
+  </template>
+  
+<style scoped>
+  .testimoni {
+    background-color: #FBBBC2;
+  }
+
+  .judul {
+    font-family: 'Carter One', cursive;
+  }
+  
+  .photo-row {
+    display: flex;
+  }
+  
+  .photo-row img {
+    width: 16.66667%;
+    object-fit: cover;
+  }
+</style>
+  
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
     <div>
         <Navbar />
         <slot />
+        <Testimoni/>
         <Footer />
     </div>
 </template>
